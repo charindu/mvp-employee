3744ce9f6a2bea7451123db49d96b8c2dbe08db1
charinduthiwanka <charindu87@gmail.com>
2020-10-24 16:46:00 +0800

    ENH : Initial commit

---
+++ mvp-employee-backend/src/main/java/com/govtech/MvpemployeeApplication.java
@@ -0,0 +1,13 @@
+package com.govtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpemployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvpemployeeApplication.class, args);
+    }
+
+}
---
+++ mvp-employee-backend/src/test/java/com/govtech/MvpemployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpemployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
