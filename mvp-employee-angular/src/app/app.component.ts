07a8b2e5946c9a072ebe96303440fbbcc14cb27d
charinduthiwanka <charindu87@gmail.com>
2020-10-25 18:19:06 +0800

    ENH 00 : Heading change

--- mvp-employee-angular/src/app/app.component.ts
+++ mvp-employee-angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'GovTech Mvp Employee';
+  title = 'GovTech MVP Employee';
 }
