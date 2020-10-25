28797a391dbdcd5cede69079c5740cc69aca1ce1
charinduthiwanka <charindu87@gmail.com>
2020-10-25 21:01:35 +0800

    ENH : Test Controller

--- mvp-employee-backend/src/test/java/com/govtech/controller/EmployeeControllerTest.java
+++ mvp-employee-backend/src/test/java/com/govtech/controller/EmployeeControllerTest.java
@@ -1,5 +1,7 @@
 package com.govtech.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.govtech.model.Employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,6 +21,9 @@ class EmployeeControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     void uploadUsers() throws Exception {
         RequestBuilder request = MockMvcRequestBuilders.post("/users/upload").accept(MediaType.APPLICATION_JSON_VALUE);
@@ -37,14 +42,28 @@ class EmployeeControllerTest {
 
     @Test
     void updateEmployee() throws Exception {
-
+        Employee mockEmployee = new Employee( "e002","testtest","Test Test",4000 );
+        RequestBuilder request = MockMvcRequestBuilders.patch("/users/2")
+                .content(objectMapper.writeValueAsString(mockEmployee))
+                .accept(MediaType.APPLICATION_JSON_VALUE);
+        MvcResult mvcResult = mockMvc.perform(request).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
     }
 
     @Test
-    void getEmployeeById() {
+    void getEmployeeById() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/users/2").accept(MediaType.APPLICATION_JSON_VALUE);
+        MvcResult mvcResult = mockMvc.perform(request).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
     }
 
     @Test
-    void deleteEmployee() {
+    void deleteEmployee() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.delete("/users/2").accept(MediaType.APPLICATION_JSON_VALUE);
+        MvcResult mvcResult = mockMvc.perform(request).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
     }
 }
